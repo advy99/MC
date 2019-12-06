87c06eb39e8dfcb890643973e57465fed3588e2c
Antonio David Villegas <advy99@gmail.com>
2019-12-06 14:30:05 +0100

    comenzada practica flex - leemos ordenes preprocesador, mostramos que includes y comentarios se han hecho, tanto en linea como en bloque (tengo que añadir comentarios en bloque de varias lineas)

---
+++ practica2/datos/entrada.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include "archivo.h"
+
+#define DEVOLVER 0
+
+using namespace std;
+
+/* esto es un comentario en bloque*/
+
+/**/
+
+int main(int argc, char ** argv){
+
+	// imprimimos hola mundo
+	cout << "hola mundo " << endl;
+
+	/*esto es otro
+	  comentario en bloque
+	  grande
+	 */
+
+	return DEVOLVER; // acabamos la funcion
+
+
+}
